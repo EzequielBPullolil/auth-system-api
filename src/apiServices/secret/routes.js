7645bdaab7b862f6a7b0d3448a4ebcb561205693
EzequielK-source <programacionezequielk@gmail.com>
2022-01-11 18:23:28 -0300

    Create secret route for AuthUsers

---
+++ src/apiServices/secret/routes.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const express = require("express");
+const router  = express.Router();
+const TokenAuthenticator = require("src/middlewares/TokenAuthenticator")
+
+router.get("/",TokenAuthenticator,(req,res)=>{
+	return res.send(`hi user ${req.user}`)
+})
