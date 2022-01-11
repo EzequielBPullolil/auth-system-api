408746ecc179dca43cd2ec578d3012772e002010
EzequielK-source <programacionezequielk@gmail.com>
2022-01-10 23:19:43 -0300

    Adapt UserRoutes to UserController changes

--- src/apiServices/user/routes.js
+++ src/apiServices/user/routes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router  = express.Router();
+const {sequelize} = require('src/services/sequelize/index');
+
+
 const UserController = require("./controller");
-const {userModel} = require("src/services/sequelize/index")
+const UserModel 	 = require("./class/UserModel")
+const userModel      = new UserModel(sequelize);
 const userController = new UserController( userModel );
 router.get("/", async (req,res)=>{
 	const {username, password} = req.body;
@@ -19,7 +23,7 @@ router.post("/", async(req,res)=>{
 	if( username == undefined || username == "" ) return res.status(400).json( {error: "need a username"} )
 	if( password == undefined || password == "" ) return res.status(400).json( {error: "need a password"} )
 
-	const userCreated = await userController.create({
+	const userCreated = await userController.createUser({
 		username,
 		password
 	})
