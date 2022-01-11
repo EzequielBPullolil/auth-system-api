4c1a7396371d423a1b68a8d7f7cac751ee9fd4dd
EzequielK-source <programacionezequielk@gmail.com>
2022-01-10 23:14:57 -0300

    Use UserModel in createUser and authUser controller class

--- src/apiServices/user/controller.js
+++ src/apiServices/user/controller.js
@@ -3,7 +3,7 @@ const User = require("./user")
 const UserUUID = require("./class/UserUUID")
 const AlreadyTakeUsername = require("./exceptions/AlreadyTakeUsername")
 const UserParamsValidator = require("./class/UserParamsValidator")
-const UsernameNotExist 		= require("src/apiServices/user/exceptions/UsernameNotExist");
+const UsernameNotExist 	  = require("src/apiServices/user/exceptions/UsernameNotExist");
 class UserController {
 	constructor(userModel) {
 		this.model = userModel;
@@ -13,15 +13,10 @@ class UserController {
 		if(!user_id){
 			user_id = new UserUUID().value
 		}
-		new UserParamsValidator({user_id,username,password});
+		new UserParamsValidator({username,password});
 		// find user with same name
-		const user = await this.model.findOne({
-			where:{
-				username:`${username}%`
-			}
-		})
+		const user = await this.model.findOneByUsername(username);
 		if(user != null) throw new AlreadyTakeUsername();
-
 		const userCreated = await this.model.create({
 			user_id:user_id,
 			username:username,
@@ -31,19 +26,13 @@ class UserController {
 		return new User({user_id, username, password});
 	}
 	async authUser(userParams){
-		const {username, password} = userParams;
-		const user = await this.model.findOne({
-			where:{
-				username:`${username}%`,
-				password:`${password}%`
-			}
-		});
+		let {username, password} = userParams;
+		const user = await this.model.findOneByUsername(username)
 		if(user == null) throw new UsernameNotExist()
-
-
+		
 	  	return JwtGenerator({
-			user_id: user.user_id,
-			username: user.username
+			user_id:user.getId(),
+			username:user.username
 		})
 	}
 }
