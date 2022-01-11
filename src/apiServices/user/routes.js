1e58420865a6266baeb21f2f5e29c9220c9f0192
EzequielK-source <programacionezequielk@gmail.com>
2022-01-11 18:04:06 -0300

    First functional state
    add try catch to UserRoutes / get

--- src/apiServices/user/routes.js
+++ src/apiServices/user/routes.js
@@ -8,15 +8,23 @@ const UserModel 	 = require("./class/UserModel")
 const userModel      = new UserModel(sequelize);
 const userController = new UserController( userModel );
 router.get("/", async (req,res)=>{
-	const {username, password} = req.body;
-	// TODO: Make class for this process
-	if( username == undefined || username == "" ) return res.status(400).json( {error: "need a username"} )
-	if( password == undefined || password == "" ) return res.status(400).json( {error: "need a password"} )
+	try{
+		const {username, password} = req.body;
+		// TODO: Make class for this process
+		if( username == undefined || username == "" ) return res.status(400).json( {error: "need a username"} )
+		if( password == undefined || password == "" ) return res.status(400).json( {error: "need a password"} )
 
-	// TODO: Make class for verify user exist
-	const userAuth = await userController.authUser({username, password});
+		// TODO: Make class for verify user exist
+		const userAuth = await userController.authUser({username, password});
 
-	return res.json(userAuth)
+		return res.json(userAuth)
+	}catch(err){
+		return res.json({
+			error: err.name,
+			description: err.message
+		}).status(400)
+	}
+
 })
 router.post("/", async(req,res)=>{
 	try{
@@ -33,7 +41,10 @@ router.post("/", async(req,res)=>{
 			username:userCreated.username
 		}).status(200);
 	}catch(err){
-		return res.json(err).status(400)
+		return res.json({
+			error: err.name,
+			description: err.message
+		}).status(400)
 	}
 })
 
