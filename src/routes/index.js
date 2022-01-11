cafac77db7c481481c3551a125cbe6bef4076df0
EzequielK-source <programacionezequielk@gmail.com>
2022-01-11 18:24:29 -0300

    Add SecretRouter to express routing

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router  = express.Router();
 const userRouter = require("src/apiServices/user/routes");
+const secretRouter = require("src/apiServices/secret/routes");
 
 
 router.use("/user", userRouter)
+router.use("/secret", secretRouter)
 
 
 
